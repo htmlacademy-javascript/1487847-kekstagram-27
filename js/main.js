4f604c277d0f35705f850a48178d7a980a32d925
OlegMikhaylovE <103054177+OlegMikhaylov0001@users.noreply.github.com>
2022-11-01 18:46:40 +0400

    more detail

--- js/main.js
+++ js/main.js
@@ -13,3 +13,80 @@ function randomInteger(min, max) {
 }
 randomInteger (1, 8);
 // источник learn.javascript.ru
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!'
+];
+
+const NAME_OF_USERS = [
+  'Георгий',
+  'Руслан',
+  'Анна',
+  'Лидия',
+  'Никита',
+  'Кристина',
+  'Иван',
+  'Роберт',
+  'София',
+  'Александр'
+];
+
+const MESSAGE_QUANTITY = {
+  MIN: 1,
+  MAX: 2,
+};
+
+const COMMENTS_QUANTITY = {
+  MIN: 1,
+  MAX: 10,
+};
+const PICTURE_QUANTITY = 25;
+const AVATAR_QUANTITY = 6;
+
+const LIKES_QUANTITY = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const getRandomArrayElement = (array) => array[randomInteger(0, array.length - 1)];
+
+const createMessage = () => Array.from({ length: randomInteger(MESSAGE_QUANTITY.MIN, MESSAGE_QUANTITY.MAX) }, () => getRandomArrayElement(COMMENTS)).join(' ');
+
+const generateComments = () => {
+  const array = [];
+  for (let i = 0; i <= randomInteger(COMMENTS_QUANTITY.MIN, COMMENTS_QUANTITY.MAX); i++) {
+
+    const commentItem = {
+      id: i,
+      avatar: `img/avatar-${randomInteger(1, AVATAR_QUANTITY)}.svg`,
+      message : createMessage(),
+      name: NAME_OF_USERS[randomInteger(0, NAME_OF_USERS.length - 1)]
+    };
+    array.push(commentItem);
+  }
+  return array;
+};
+
+const descriptionImage = (i) => ({
+  id: i,
+  url:`photos/${i}.jpg`,
+  description: 'Photo Like',
+  likes: randomInteger(LIKES_QUANTITY.MIN, LIKES_QUANTITY.MAX),
+  comments: generateComments(),
+});
+
+const arrayOfObjects = () => {
+  const array = [];
+  for (let i = 1; i <= PICTURE_QUANTITY; i++) {
+    array.push(descriptionImage(i));
+  }
+  return array;
+};
+arrayOfObjects();
+generateComments();
